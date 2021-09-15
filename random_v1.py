63123913decbbd14b40bfdbfa8f3ceba6348ac87
holman.april <holman.april@awatapu.school.nz>
2021-09-15 15:04:04 +1200

    First version of yes_no checker

---
+++ random_v1.py
@@ -0,0 +1,10 @@
+import random
+number = random.randint(1, 5)
+print(number)
+
+
+numbers = ["1", "2", "3", "4", "5", "6"]
+print(numbers)
+pick = 4
+numbers.remove("{}".format(pick))
+print(numbers)
