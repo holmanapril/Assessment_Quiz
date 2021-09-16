842d643eab3a0fcacadd88435eaec3742ca3f241
holman.april <holman.april@awatapu.school.nz>
2021-09-17 09:58:24 +1200

    added comments

--- how_many_questions_v1.py
+++ how_many_questions_v1.py
@@ -1,5 +1,8 @@
+# Asks how many questions user would like to play
 question_amount = int(input("How many questions would you like to play? 10, 15 or 20?"))
+# If questions_amount is equal to 10, 15 or 20 it print it
 if question_amount == 10 or question_amount == 15 or question_amount == 20:
     print("You chose {} rounds".format(question_amount))
+# If questions_amount it not one of the choices print please enter 10, 15 or 20
 else:
     print("Please enter 10, 15 or 20")
