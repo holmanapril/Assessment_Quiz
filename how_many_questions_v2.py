8335bf4eb64fa72c7b31a89b8e3d1c53e8204acc
holman.april <holman.april@awatapu.school.nz>
2021-09-20 12:48:30 +1200

    version that works for any input and loops

---
+++ how_many_questions_v2.py
@@ -0,0 +1,13 @@
+error = "Please enter 10, 15 or 20"
+
+valid = False
+while not valid:
+    try:
+        # Ask question
+        question_amount = int(input("How much would you like to play with? "))
+        if question_amount == 10 or question_amount == 15 or question_amount == 20:
+            print("You chose to play {} rounds".format(question_amount))
+        else:
+            print(error)
+    except ValueError:
+        print(error)
