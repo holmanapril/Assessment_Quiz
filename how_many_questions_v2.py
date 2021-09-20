37c7d754a4e0b73c182820c88b991f8762c8d5ea
holman.april <holman.april@awatapu.school.nz>
2021-09-20 13:09:43 +1200

    put all the comments in

--- how_many_questions_v2.py
+++ how_many_questions_v2.py
@@ -1,12 +1,16 @@
+# error statement for whenever the user enters an invalid input
 error = "Please enter 10, 15 or 20"
-
+# Loop
 valid = False
 while not valid:
+    # Check if input entered is valid
     try:
-        # Ask question
+        # Ask how many rounds user wants to play
         question_amount = int(input("How many questions would you like to play? 10, 15 or 20?"))
+        # If questions_amount is equal to 10, 15 or 20 it print it
         if question_amount == 10 or question_amount == 15 or question_amount == 20:
             print("You chose to play {} rounds".format(question_amount))
+        # If questions_amount it not one of the choices print please enter 10, 15 or 20
         else:
             print(error)
     except ValueError:
--- how_many_questions_v4.py
+++ how_many_questions_v4.py
@@ -1,15 +1,19 @@
 # Functions
 def how_many_questions(question, choice_1, choice_2, choice_3):
+    # error statement for whenever the user enters an invalid input
     error = "Please enter 10, 15 or 20"
-
+    # Loop
     valid = False
     while not valid:
+        # Check if input entered is valid
         try:
-            # Ask question
+            # Ask how many rounds user wants to play
             response = int(input(question))
+            # If questions_amount is equal to 10, 15 or 20 it print it
             if response == choice_1 or response == choice_2 or response == choice_3:
                 print("You chose to play {} rounds".format(response))
                 return response
+            # If questions_amount it not one of the choices print please enter 10, 15 or 20
             else:
                 print(error)
         except ValueError:
