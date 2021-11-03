c4135ddd51fe50a7bc7c119801141c85adc9ca7c
holman.april <holman.april@awatapu.school.nz>
2021-11-04 09:49:56 +1300

    slightly changed the you have chosen __ rounds to questions

--- statement_decorator_v1.py
+++ statement_decorator_v1.py
@@ -15,7 +15,7 @@ def how_many_questions(question, choice_1, choice_2, choice_3):
             response = int(input(question))
             # If questions_amount is equal to 10, 15 or 20 it print it
             if response == choice_1 or response == choice_2 or response == choice_3:
-                print("You chose to play {} rounds".format(response))
+                print("You chose to play {} questions".format(response))
                 print()
                 return response
             # If questions_amount it not one of the choices print please enter 10, 15 or 20
@@ -78,6 +78,8 @@ def ask_questions():
     print("Final Score = {}/{}".format(score, amount_questions_asked - 1))
     print()
     print("Great Job!")
+    print()
+    decoration("Thank You for playing", "*")
 
 
 def decoration(greeting, symbol):
